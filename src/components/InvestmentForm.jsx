aa57e11b305566e45855d96f865bccf2837769fc
RehanGeorge <rehan.george@gmail.com>
2023-12-17 01:44:45 +0530

    Added Investment Form and Cast all values to Numbers in the Investment calculator

--- src/components/InvestmentForm.jsx
+++ src/components/InvestmentForm.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react"
+import { calculateInvestmentResults } from "../util/investment"
+
 import InvestmentFormFields from "./InvestmentFormFields"
 
 export default function InvestmentForm() {
+    const [formDetails, setFormDetails] = useState({initialInvestment: 0, annualInvestment: 0, expectedReturn: 0, duration: 0})
+
+    function formChangeHandler(target, value) {
+        setFormDetails((prev) => {
+            return {...prev, [target]: value}
+        })
+    }
+
+    const investmentResults = calculateInvestmentResults(formDetails);
+    console.log(investmentResults);
+    
     return (
         <div id="user-input">
             <div className="input-group">
-            <InvestmentFormFields label="Initial Investment" />
-            <InvestmentFormFields label="Annual Investment" />
+            <InvestmentFormFields label="Initial Investment" onInput={formChangeHandler}/>
+            <InvestmentFormFields label="Annual Investment" onInput={formChangeHandler}/>
             </div>
             <div className="input-group">
-            <InvestmentFormFields label="Expected Return" />
-            <InvestmentFormFields label="Duration" />
+            <InvestmentFormFields label="Expected Return" onInput={formChangeHandler}/>
+            <InvestmentFormFields label="Duration" onInput={formChangeHandler}/>
             </div>
         </div>
     )
--- src/components/InvestmentFormFields.jsx
+++ src/components/InvestmentFormFields.jsx
@@ -1,8 +1,18 @@
-export default function InvestmentFormFields({ label }) {
+import { useState } from "react"
+
+export default function InvestmentFormFields({ label, onInput }) {
+    const [fieldValue, setFieldValue] = useState(0);
+
+    function valueChangeHandler(event) {
+        setFieldValue(event.target.value);
+        const id = label.replace(/\s+/g, ' ').split(' ').map((word, index) => index === 0 ? word.toLowerCase() : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('');
+        onInput(id, event.target.value);
+    }
+
     return (
         <div>
             <label htmlFor={label}>{label}</label>
-            <input type="number" id={label} />
+            <input type="number" id={label} value={fieldValue} onChange={valueChangeHandler}/>
         </div>
     )
 }
